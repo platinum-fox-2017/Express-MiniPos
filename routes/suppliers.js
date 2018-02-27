f36f86be0bdf00b960d91f031f34f39e75e25fab
WikaSilo <wikanyaa@gmail.com>
2018-02-28 00:13:04 +0700

    hooknya belum

--- routes/suppliers.js
+++ routes/suppliers.js
@@ -1,6 +1,7 @@
 'use strict'
 const router = require('express').Router();
 const models = require('../models');
+const Op = require('sequelize').Op;
 
 router.get('/', (req, res) => {
     models.Supplier.findAll({
@@ -81,28 +82,49 @@ router.get('/:id/additem', (req, res) => {
     // res.send('On developed')
     // res.send(req.params.id)
     models.Supplier.findAll({
-        include: [
-            {model: models.SupplierItem},
-            {model: models.Item}
-        ],
-        where: {id: req.params.id}
-    })
-    .then((dataSupplier) => {
-        models.Item.findAll()
-        .then((items) =>{
-            res.render('./suppliers/addItemSupplier.ejs', {dataSupplier: dataSupplier, items:items})
-            // res.send(items)
+        include: [{
+        model: models.Item
+        }],
+        where: {
+        id: req.params.id
+        }
+    }).then(dataSupplier => {
+        let arr = []
+        dataSupplier[0].Items.forEach(item => {
+            arr.push(item.id)
+        });
+        models.Item.findAll({
+            where: {
+            id: {[Op.notIn]: arr}
+            }
         })
-        .catch((err) => {
-            res.send(err);
+        .then(items =>{
+            // res.send(dataSupplier[0])
+            res.render('./suppliers/addItemSupplier.ejs', {dataSupplier: dataSupplier[0], items: items})
         })
-        // res.send(dataSupplier)
-        // console.log(dataSupplier);
+        .catch(err => {
+            console.log(err)
+        })
+    }).catch(err => {
+        console.log(err)
     })
-    .catch((err) => {
-        res.send(err);
-    })
+})
 
+router.post('/:id/additem', (req, res) => {
+  // res.send(req.body)
+  // console.log(req.params.id)
+  let obj = {
+    SupplierId: req.params.id,
+    ItemId: req.body.ItemId,
+    price: req.body.price
+  }
+  // res.send(obj)
+  models.SupplierItem.create(obj)
+  .then(() => {
+    res.redirect(`/suppliers/${obj.SupplierId}/additem`)
+  }).catch(err => {
+    console.log(err)
+  })
 })
 
 module.exports = router;
